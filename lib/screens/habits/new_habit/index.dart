41c353e0f7d3f5506bc3f4e51024a53e07e0689a
Melvin Davis <melvinodsa@gmail.com>
2021-06-18 23:43:54 +0530

    Styled the category items

--- lib/screens/habits/new_habit/index.dart
+++ lib/screens/habits/new_habit/index.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:habit/components/buttons/expandable_fab.dart';
 import 'package:habit/config/index.dart';
-import 'package:habit/config/theme.dart';
-import 'package:habit/constants/inspirational_messages.dart';
+import 'package:habit/models/category_item.dart';
 
 class NewHabitWizardCategory extends StatefulWidget {
   NewHabitWizardCategory({Key? key, required this.config}) : super(key: key);
@@ -15,6 +12,18 @@ class NewHabitWizardCategory extends StatefulWidget {
 }
 
 class _NewHabitWizardCategoryState extends State<NewHabitWizardCategory> {
+  List<CategoryItem> list1 = [
+    CategoryItem(
+      label: 'Quit a bad habit',
+      icon: Icons.block,
+      color: Colors.red,
+    ),
+    CategoryItem(label: 'Study', icon: Icons.school, color: Colors.purple),
+  ];
+  List<CategoryItem> list2 = [
+    CategoryItem(
+        label: 'Sports', icon: Icons.directions_bike, color: Colors.green),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,52 +36,80 @@ class _NewHabitWizardCategoryState extends State<NewHabitWizardCategory> {
   Row _buildBody() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(
-          children: [
-            Container(
-                margin: EdgeInsets.only(
-                    left: 20.0, top: 20.0, right: 20.0, bottom: 20.0),
-                child: Card(
-                    child: InkWell(
-                        splashColor: Colors.blue.withAlpha(30),
-                        onTap: () {
-                          print('Card tapped.');
-                        },
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width / 2 - 60,
-                          child: Container(
-                              padding: EdgeInsets.only(
-                                  left: 20.0,
-                                  top: 10.0,
-                                  right: 20.0,
-                                  bottom: 10.0),
-                              child: Text('Quit a bad habit')),
-                        )))),
-          ],
-        ),
-        Column(children: [
+      children: [list1, list2]
+          .map((list) => SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Column(
+                  children: list
+                      .map((e) => CategoryItemWidget(context: context, data: e))
+                      .toList(),
+                ),
+              ))
+          .toList(),
+    );
+  }
+}
+
+class CategoryItemWidget extends StatelessWidget {
+  const CategoryItemWidget({
+    Key? key,
+    required this.context,
+    required this.data,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final CategoryItem data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2 - 40,
+      margin: EdgeInsets.all(10.0),
+      child: Row(
+        children: [
           Container(
-              margin: EdgeInsets.only(
-                  left: 20.0, top: 20.0, right: 20.0, bottom: 20.0),
-              child: Card(
-                  child: InkWell(
-                      splashColor: Colors.blue.withAlpha(30),
-                      onTap: () {
-                        print('Card tapped.');
-                      },
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width / 2 - 60,
-                        child: Container(
-                            padding: EdgeInsets.only(
-                                left: 20.0,
-                                top: 10.0,
-                                right: 20.0,
-                                bottom: 10.0),
-                            child: Text('Meditation')),
-                      )))),
-        ]),
-      ],
+            width: 2,
+            height: 45,
+            decoration: BoxDecoration(color: this.data.color),
+            child: Text(''),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 2 - 42,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(5),
+                      bottomRight: Radius.circular(5))),
+              margin: EdgeInsets.all(0),
+              child: InkWell(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(100), topLeft: Radius.zero),
+                  splashColor: this.data.color.withAlpha(30),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: SizedBox(
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            this.data.label,
+                            style: TextStyle(fontSize: 10),
+                          ),
+                          Icon(
+                            this.data.icon,
+                            color: this.data.color,
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
